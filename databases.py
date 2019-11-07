39444c8828c60a999bd87de84fe3d6724fc168e7
myname21-meet <myname21@meet.mit.edu>
2019-11-07 18:58:59 +0200

    yay

---
+++ databases.py
@@ -0,0 +1,56 @@
+from model import Base, Student
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+# replace lecture.db with your own database file
+engine = create_engine('sqlite:///lecture.db')
+Base.metadata.create_all(engine)
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+def add_product(name, price, Picture_Link,Description):
+
+    product = Product(
+        name=name,
+        price=price,
+        Picture_Link=Picture_Link,
+        Description=Description)
+    session.add(product_object)
+    session.commit()
+
+def update_product(id, price):
+   
+   product_object = session.query(
+       Product).filter_by(
+       id=id).first()
+   product_object.price = price
+   session.commit()
+
+def delete_student(their_id):
+
+   session.query(Product).filter_by(
+       id=their_id).delete()
+   session.commit()
+
+def query_all():
+
+   products = session.query(
+      Product).all()
+   return products
+
+def query_by_id(their_id):
+
+   product = session.query(
+       Product).filter_by(
+       id=their_id).first()
+   return product
+
+ def add_To_Cart(productID):
+
+    carts = Cart(
+        productID=productID)
+    session.add(carts)
+    session.commit()
